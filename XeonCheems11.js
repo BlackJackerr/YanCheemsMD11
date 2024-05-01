1f9c5697aa73366260f9ee11160bcdead458d5a7
BlackJackerr <125628537+BlackJackerr@users.noreply.github.com>
2024-05-01 17:08:42 +0800

    Add files via upload

--- XeonCheems11.js
+++ XeonCheems11.js
@@ -3180,14 +3180,14 @@ break
             case 'smeme': {
             let media = await XeonBotInc.downloadAndSaveMediaMessage(qmsg)
             const swn = args.join(" ")
-            const pcknm = swn.split("|")[0] : ''
-            const atnm = swn.split("|")[1] : ''
+            const pcknm = swn.split("|")[0] ? swn.split("|")[0] : '';
+            const atnm = swn.split("|")[1] ? swn.split("|")[1] : '';
             let mem = await TelegraPh(media)
             ryangk = await getBuffer(`https://api.memegen.link/images/custom/${pcknm}/${atnm}.png?background=${mem}`)
             await XeonBotInc.sendImageAsSticker(m.chat, ryangk, m, { packname: global.packname, author: global.author })
             }
             break
-            
+           
             case 'emojimix': {
                 let [emoji1, emoji2] = text.split`+`
                 if (!emoji1) return replygcxeon(`Example : ${prefix + command} 😅+🤔`)
