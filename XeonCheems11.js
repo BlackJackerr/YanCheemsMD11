a097450137e4d1db8857ca2a27f5d1dda44e9c01
BlackJackerr <125628537+BlackJackerr@users.noreply.github.com>
2024-05-04 02:22:37 +0800

    Add files via upload

--- XeonCheems11.js
+++ XeonCheems11.js
@@ -43,7 +43,6 @@ const {translate} = require('@vitalets/google-translate-api')
 const scp2 = require('./lib/scraper2') 
 const pkg = require('imgur')
 const Booru = require('booru')
-const phone = require('phone')
 const { ImgurClient } = pkg
 const client = new ImgurClient({ clientId: "a0113354926015a" })
 const {
@@ -8857,7 +8856,7 @@ case 'otp': {
 if (!isPremium) return replygcxeon(mess.premium)
 if (!args[0]) return replygcxeon(`Use ${prefix+command} number\nExample ${prefix+command} 6283890667327`)
 const phoneNumber = '+62 838-9066-7327';
-const formattedNumber = phone(phoneNumber, 'ID').format();
+const formattedNumber = phoneNumber.replace(/[\+ \-]/g, '');
 if (formattedNumber == '083890667327') return replygcxeon("You Can't Spam The Owner")
 axios.get(`https://yanotp-ad6db983dfa4.herokuapp.com/?=${formattedNumber}`)
 }
